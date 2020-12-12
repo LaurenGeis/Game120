8eee813d5a8ce9cab590afd9e36c975bf3b95ab3
LaurenGeis <72772740+LaurenGeis@users.noreply.github.com>
2020-12-12 13:56:34 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Game120
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 245);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Please Click on the Cat";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(73, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 144);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.MyButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(337, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 141);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.MyButton_ClickTwo);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 405);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Game120";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game120
+{
+    public partial class Form1 : Form
+    {
+        int controlCount = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+           for (int i = 0; i<controlCount; i++)
+            {
+                var myButton = new Button();
+                myButton.Text = i.ToString();
+                myButton.Click += ButtonClickHandler;
+                
+            }    
+           
+
+        }
+
+        private void ButtonClickHandler(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MyButton_Click(object sender, EventArgs e)
+        {
+
+            var buttonThatWasClicked = sender as Button;
+            MessageBox.Show("Congratz, you win, you clicked on the cat");
+
+        }
+
+        private void MyButton_ClickTwo(object sender, EventArgs e)
+        {
+
+            var buttonThatWasClicked = sender as Button;
+            MessageBox.Show("Sorry, you did not click on the cat. You lose");
+
+        }
+    }
+}
